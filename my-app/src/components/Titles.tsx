a4ca3c825b6140d71217a4398e860ae9ae0c1e67
Michael Chung <michaelchungng@live.com>
2018-09-04 01:34:28 +1200

    Titles.tsx created

---
+++ my-app/src/components/Titles.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class Titles extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <p>Find your local weather temperature, conditions and more...</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;

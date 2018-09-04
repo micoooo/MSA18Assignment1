7148d7435dfa320152a88037d301a3ff2ad836c2
Michael Chung <michaelchungng@live.com>
2018-09-04 17:24:53 +1200

    weather icons added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,12 +4,28 @@ import Form from "./components/Form"
 import Titles from "./components/Titles"
 import Weather from "./components/Weather"
 
+const API_KEY = "855dcc486ca0e07e59b3c6ffcac0fcd6";
+
+
 class App extends React.Component {
+  constructor(props:any) {
+    super(props);
+    this.getWeather = this.getWeather.bind(this)
+  }
+  getWeather() {
+    console.log(this.props);
+  }
+  private getWeather = async (event:any) => {
+    event.preventDefault();
+    const apiCall = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric');
+    const data = await apiCall.json();
+    console.log(data);
+  }
   public render() {
     return (
       <div className="App">
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
--- my-app/src/components/Form.tsx
+++ my-app/src/components/Form.tsx
@@ -3,7 +3,11 @@ import * as React from 'react';
 class Form extends React.Component {
     public render() {
         return(
-            <div>Form Component</div>
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" placeholder="city..."/>
+                <input type="text" name="country" placeholder="country..."/>
+                <button>Get Weather</button>
+            </form>
         );
     }
 }

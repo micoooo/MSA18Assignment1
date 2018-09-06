a5f4b0551247d7a3f6335825e8f3a4f678d9c8d8
Michael Chung <michaelchungng@live.com>
2018-09-06 15:50:29 +1200

    WeeklyWeather modified

--- my-app/src/components/WeatherDetails.tsx
+++ my-app/src/components/WeatherDetails.tsx
@@ -3,65 +3,65 @@ import { withStyles, createStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
 const styleSheet = createStyles({
-    flex: {
-        display: 'flex',
-    },
-    space: {
-        marginRight: 8,
-    }
+  flex: {
+    display: 'flex',
+  },
+  space: {
+    marginRight: 8,
+  }
 
 });
 
-function WeatherDetails(props:any) {
-    const classes = props.classes;
-    const result = props.data
+function WeatherDetails(props: any) {
+  const classes = props.classes;
+  const result = props.data
 
-    const humidity = Math.round(result.humidity)
-    const windSpeed = result.speed
-    const windDegree = result.deg
-    const cloudiness = Math.round(result.clouds)
-    const pressure = Math.round(result.pressure)
+  const humidity = Math.round(result.humidity)
+  const windSpeed = result.speed
+  const windDegree = result.deg
+  const cloudiness = Math.round(result.clouds)
+  const pressure = Math.round(result.pressure)
 
-    return (
-        <div className={classes.root}>
-            <div className={classes.flex}>
-                <Typography variant="display1" className={classes.space} >
-                    Humidity:
+  return (
+    <div className={classes.root}>
+      <div className={classes.flex}>
+        <Typography variant="display1" className={classes.space} >
+          Humidity:
         </Typography>
-                <Typography variant="display1">
-                    {`${humidity}%`}
-                </Typography>
-            </div>
-
-            <div className={classes.flex}>
-                <Typography variant="display1" className={classes.space} >
-                    Wind:
+        <Typography variant="display1">
+          {`${humidity}%`}
         </Typography>
-                <Typography variant="display1"  >
-                    {`${windSpeed} m/s ${windDegree}°`}
-                </Typography>
-            </div>
+      </div>
 
-            <div className={classes.flex}>
-                <Typography variant="display1" className={classes.space} >
-                    Cloudiness:
+      <div className={classes.flex}>
+        <Typography variant="display1" className={classes.space} >
+          Wind:
         </Typography>
-                <Typography variant="display1"  >
-                    {`${cloudiness} %`}
-                </Typography>
-            </div>
-            <div className={classes.flex}>
-                <Typography variant="display1" className={classes.space} >
-                    Pressure:
+        <Typography variant="display1"  >
+          {`${windSpeed} m/s ${windDegree}°`}
         </Typography>
-                <Typography variant="display1"  >
-                    {`${pressure} hPa`}
-                </Typography>
-            </div>
+      </div>
+
+      <div className={classes.flex}>
+        <Typography variant="display1" className={classes.space} >
+          Cloudiness:
+        </Typography>
+        <Typography variant="display1"  >
+          {`${cloudiness} %`}
+        </Typography>
+      </div>
+      <div className={classes.flex}>
+        <Typography variant="display1" className={classes.space} >
+          Pressure:
+        </Typography>
+        <Typography variant="display1"  >
+          {`${pressure} hPa`}
+        </Typography>
+      </div>
 
 
-        </div>
-    );
+    </div>
+  );
 }
 
 export default withStyles(styleSheet)(WeatherDetails)
---
+++ my-app/src/components/WeeklyWeather.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { withStyles, createStyles } from '@material-ui/core/styles';
+import DailyWeather from './DailyWeather'
+
+const styleSheet = (theme: any) => createStyles(({
+  daily: {
+    justifyContent: "space-between",
+    display: "flex",
+    marginRight: 32
+  }
+}))
+
+function WeeklyWeather(props:any) {
+  const classes = props.classes;
+  const result = props.data
+  let days
+  result.shift()
+
+  days = result.map((day:any) =>
+    <DailyWeather data={day} key={day.dt} />
+  )
+
+  return (
+    <ul className={classes.daily}>
+    {days}
+    </ul>
+  )
+}
+
+export default withStyles(styleSheet)(WeeklyWeather);

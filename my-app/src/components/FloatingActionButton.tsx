f5fed9771ea2c3caff2c852bbb89b37982806656
Michael Chung <michaelchungng@live.com>
2018-09-06 15:42:06 +1200

    FloatingActionButton modified

--- my-app/src/components/FloatingActionButton.tsx
+++ my-app/src/components/FloatingActionButton.tsx
@@ -1,1 +1,28 @@
-asdasds
+import * as React from 'react'
+import { withStyles, createStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/core/Typography';
+
+const styleSheet = (theme:any) => createStyles(({
+    button: {
+        margin: theme.spacing.unit,
+        marginLeft: "auto",
+        display: "block",
+    },
+}));
+
+function FloatingActionButtons(props:any) {
+    const classes = props.classes;
+    return (
+        <div>
+            <Button
+                color="primary"
+                aria-label="add"
+                className={classes.button}>
+                <AddIcon />
+            </Button>
+        </div>
+    );
+}
+
+export default withStyles(styleSheet)(FloatingActionButtons);
